dbc6cff2082c9845306f9e3cdda812ad9deecf72
Taeyoung Won <hexter1994@gmail.com>
2024-02-23 18:37:18 +0900

    [Build] Realm Swift 의존성 추가

--- Package.swift
+++ Package.swift
@@ -11,11 +11,17 @@ let package = Package(
             name: "KazRealm",
             targets: ["KazRealm"]),
     ],
+    dependencies: [
+      .package(url: "https://github.com/realm/realm-swift", from: Version(10, 47, 0))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KazRealm"),
+            name: "KazRealm",
+            dependencies: [
+              .product(name: "RealmSwift", package: "realm-swift")
+            ]),
         .testTarget(
             name: "KazRealmTests",
             dependencies: ["KazRealm"]),
